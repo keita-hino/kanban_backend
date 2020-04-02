02ea1024e3370a898ba460736d4f3f45cd19c614
日野恵太 <keitahino0415@gmail.com>
2020-04-02 23:12:07 +0900

    tasksテーブル追加

---
+++ db/migrate/20200402140709_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.integer :status
+      t.date :start_date
+      t.date :due_date
+      t.integer :display_order
+
+      t.timestamps
+    end
+  end
+end
