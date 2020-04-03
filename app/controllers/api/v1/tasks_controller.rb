fa513190fb845ac59d0f08c07122d06eb64169d2
日野恵太 <keitahino0415@gmail.com>
2020-04-03 22:46:20 +0900

    タスクのステータスを変更 する機能追加

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -15,6 +15,38 @@ class Api::V1::TasksController < ApplicationController
     render json: { tasks: @tasks }
   end
 
+  # ステータス更新用アクション
+  def update_status_task
+    # 挿入されたタスク直下のタスク
+    before_moved_task = Task.where(status: update_status_task_params[:status])
+                            .where(display_order: update_status_task_params[:display_order])
+                            .order(:display_order)
+                            .last
+
+    # タスクステータス更新処理
+    task = Task.find(update_status_task_params[:id])
+    task.assign_attributes(update_status_task_params)
+    task.save!
+
+    # 先頭のタスクだった場合は新たに作成したdisplay_order
+    target_display_order = before_moved_task.nil? ? task.display_order : before_moved_task.display_order
+
+    # 追加したタスクより後のタスク取得
+    target_tasks = Task.where(status: update_status_task_params[:status])
+                       .where('display_order >= ?', target_display_order)
+                       .where.not(id: task.id)
+                       .order(:display_order)
+
+    # 並び更新
+    target_tasks.each.with_index(1) do |target_task, index|
+      target_task.display_order = task.display_order + index
+      target_task.save!
+    end
+
+    @tasks = Task.order(:display_order)
+    render json: { tasks: @tasks }
+  end
+
   private
 
   # タスクの並び更新用
@@ -23,8 +55,18 @@ class Api::V1::TasksController < ApplicationController
     params.permit(
       tasks: [
         :id,
+        :status,
         :display_order
       ]
+    )
+  end
+
+  # タスクのステータス更新用のアクション
+  def update_status_task_params
+    params.require(:task).permit(
+      :id,
+      :status,
+      :display_order
     )
   end
 
