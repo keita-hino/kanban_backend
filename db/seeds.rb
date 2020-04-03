f7ad24f7747e6f5c632f0db288df330360444a26
日野恵太 <keitahino0415@gmail.com>
2020-04-03 23:46:06 +0900

    リファクタリング

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,20 @@
 # end
 
 10.times do |i|
+  status = 1
+  if i > 3 && i <= 6
+    status = 2
+  elsif i >= 7
+    status = 3
+  end
+
+  display_order = Task.where(status: status).count + 1
+
   Task.create(
     name: "タスク#{i}",
-    status: rand(1..Task.statuses.count),
+    status: status,
     start_date: "2019/3/#{1 + i}",
     due_date: "2019/3/#{10 + i}",
-    display_order: 1 + i
+    display_order: display_order
   )
 end
