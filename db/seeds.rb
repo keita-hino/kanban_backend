2d90cf5937e9d5228dde4703791afb7951b61ed8
日野恵太 <keitahino0415@gmail.com>
2020-04-03 09:56:46 +0900

    Taskのstatusをランダムで生成するように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 10.times do |i|
   Task.create(
     name: "タスク#{i}",
-    status: 1,
+    status: rand(1..Task.statuses.count),
     start_date: "2019/3/#{1 + i}",
     due_date: "2019/3/#{10 + i}",
     display_order: 1 + i
