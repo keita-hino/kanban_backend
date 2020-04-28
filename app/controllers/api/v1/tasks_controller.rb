bfbc26a66ca82c1d8955ab6ef9bbea533ee0ec85
日野恵太 <keitahino0415@gmail.com>
2020-04-28 13:46:21 +0900

    タスクのステータス更新用のサービスクラス作成

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -40,7 +40,6 @@ class Api::V1::TasksController < ApplicationController
   end
 
   # タスクの並び順を変更するタスク
-  # TODO:リファクタリング
   def moved_tasks
     workspace_id = params[:workspace_id]
     task_mover = TaskMover.new(workspace_id, moved_tasks_params)
@@ -51,53 +50,10 @@ class Api::V1::TasksController < ApplicationController
   end
 
   # ステータス更新用アクション
-  # TODO:リファクタリング
   def update_status_task
     workspace_id = params[:workspace_id]
-
-    # 挿入されたタスク直下のタスク
-    before_moved_task = Task.status_is(moved_tasks_params[:status])
-                            .workspace_id_is(workspace_id)
-                            .display_order_is(moved_tasks_params[:display_order])
-                            .order(:display_order)
-                            .last
-
-    # タスクステータス更新処理
-    task = Task.find(moved_tasks_params[:id])
-    task.assign_attributes(moved_tasks_params)
-    task.save!
-
-    # 同一ステータスの末尾タスクのID取得
-    last_task_id = Task.status_is(moved_tasks_params[:status])
-                       .workspace_id_is(workspace_id)
-                       .order(:display_order)
-                       .last
-                       .id
-
-    # 先頭のタスクだった場合は新たに作成したdisplay_order
-    target_display_order = before_moved_task.nil? ? task.display_order : before_moved_task.display_order
-
-    # 末尾に追加された場合は更新しない
-    if !before_moved_task.nil? && before_moved_task&.id != last_task_id
-      # 追加したタスクより後のタスク取得
-      target_tasks = Task.status_is(moved_tasks_params[:status])
-                         .workspace_id_is(workspace_id)
-                         .display_order_ge(target_display_order)
-                         .not_id_is(task.id)
-                         .order(:display_order)
-
-      # 並び更新
-      target_tasks.each.with_index(1) do |target_task, index|
-        target_task.display_order = task.display_order + index
-        target_task.save!
-      end
-
-
-    else
-      # 末尾にタスクが追加された場合は追加したタスクのdisplay_orderをインクリメントする
-      task.display_order += 1
-      task.save!
-    end
+    task_status_updater = TaskStatusUpdater.new(workspace_id, moved_tasks_params)
+    task_status_updater.update
 
     @tasks = Task.workspace_id_is(workspace_id).order(:display_order)
     render json: { tasks: @tasks }
