7b0c35d4bf91178cc4761d0d16b30db8172fafd1
keita <keita.hino@diddyworks.co.jp>
2020-05-05 16:10:45 +0900

    tasksのcreateアクションのテスト追加

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -10,8 +10,30 @@ RSpec.describe Api::V1::TasksController, type: :controller do
 
       json = JSON.parse(response.body)
 
+      # リクエスト成功を表す200が返ってきたか確認する。
+      expect(response.status).to eq(200)
+
       # タスクの一覧を取得できるか
       expect(json['tasks'].length).to eq(10)
+    end
+  end
+
+  describe "POST #create" do
+    it "タスクの新規作成" do
+      @workspace = create(:workspace)
+
+      task_create_params = {
+        task: {
+          name: 'タスク'
+        },
+        workspace_id: @workspace.id
+      }
+
+      # リクエスト成功を表す200が返ってきたか確認する。
+      expect(response.status).to eq(200)
+
+      # タスクの新規作成処理でタスクが追加されているか確認する。
+      expect { post :create, params: task_create_params }.to change(Task, :count).by(+1)
     end
   end
 
