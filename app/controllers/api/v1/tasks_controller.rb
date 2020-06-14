8a06e8890515e31d1905bfb30977d3579505c40d
keita <keitahino0415@gmail.com>
2020-06-14 16:38:00 +0900

    rubocopの指摘修正

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,77 +1,82 @@
-class Api::V1::TasksController < ApplicationController
-  def index
-    @tasks = Task.workspace_id_is(params[:workspace_id]).order(:display_order)
-    @statuses = Task.statuses_i18n
-    @priorities = Task.priorities.keys
-    render json: { tasks: @tasks, priorities: @priorities, statuses: @statuses }
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class TasksController < ApplicationController
+      def index
+        @tasks = Task.workspace_id_is(params[:workspace_id]).order(:display_order)
+        @statuses = Task.statuses_i18n
+        @priorities = Task.priorities.keys
+        render json: { tasks: @tasks, priorities: @priorities, statuses: @statuses }
+      end
+
+      def create
+        workspace_id = params[:workspace_id]
+        Task::Creater.new(workspace_id, create_tasks_params).call
+
+        @tasks = Task.workspace_id_is(workspace_id).order(:display_order)
+        render json: { tasks: @tasks }
+      end
+
+      def update
+        workspace_id = params[:workspace_id]
+        Task::Updater.new(workspace_id, update_tasks_params).call
+
+        @tasks = Task.workspace_id_is(workspace_id).order(:display_order)
+        render json: { tasks: @tasks }
+      end
+
+      # タスクの並び順を変更するタスク
+      def moved_tasks
+        workspace_id = params[:workspace_id]
+        TaskOrderUpdater.new(workspace_id, moved_tasks_params).call
+
+        @tasks = Task.workspace_id_is(workspace_id).order(:display_order)
+        render json: { tasks: @tasks }
+      end
+
+      # ステータス更新用アクション
+      def update_status_task
+        workspace_id = params[:workspace_id]
+        TaskStatusUpdater.new(workspace_id, moved_tasks_params).call
+
+        @tasks = Task.workspace_id_is(workspace_id).order(:display_order)
+        render json: { tasks: @tasks }
+      end
+
+      private
+
+      # タスクの並び更新用
+      # @return [Object] params パラメータ
+      def moved_tasks_params
+        params.require(:task).permit(
+          :id,
+          :status,
+          :display_order
+        )
+      end
+
+      # タスクの作成用パラメータ
+      # @return [Object] params パラメータ
+      def create_tasks_params
+        params.require(:task).permit(
+          :name,
+          :status
+        )
+      end
+
+      # タスクの作成用パラメータ
+      # @return [Object] params パラメータ
+      def update_tasks_params
+        params.require(:task).permit(
+          :id,
+          :name,
+          :priority,
+          :due_date,
+          :status,
+          :detail
+        )
+      end
+    end
   end
-
-  def create
-    workspace_id = params[:workspace_id]
-    Task::Creater.new(workspace_id, create_tasks_params).call
-
-    @tasks = Task.workspace_id_is(workspace_id).order(:display_order)
-    render json: { tasks: @tasks }
-  end
-
-  def update
-    workspace_id = params[:workspace_id]
-    Task::Updater.new(workspace_id, update_tasks_params).call
-
-    @tasks = Task.workspace_id_is(workspace_id).order(:display_order)
-    render json: { tasks: @tasks }
-  end
-
-  # タスクの並び順を変更するタスク
-  def moved_tasks
-    workspace_id = params[:workspace_id]
-    TaskOrderUpdater.new(workspace_id, moved_tasks_params).call
-
-    @tasks = Task.workspace_id_is(workspace_id).order(:display_order)
-    render json: { tasks: @tasks }
-  end
-
-  # ステータス更新用アクション
-  def update_status_task
-    workspace_id = params[:workspace_id]
-    TaskStatusUpdater.new(workspace_id, moved_tasks_params).call
-
-    @tasks = Task.workspace_id_is(workspace_id).order(:display_order)
-    render json: { tasks: @tasks }
-  end
-
-  private
-
-  # タスクの並び更新用
-  # @return [Object] params パラメータ
-  def moved_tasks_params
-    params.require(:task).permit(
-      :id,
-      :status,
-      :display_order
-    )
-  end
-
-  # タスクの作成用パラメータ
-  # @return [Object] params パラメータ
-  def create_tasks_params
-    params.require(:task).permit(
-      :name,
-      :status
-    )
-  end
-
-  # タスクの作成用パラメータ
-  # @return [Object] params パラメータ
-  def update_tasks_params
-    params.require(:task).permit(
-      :id,
-      :name,
-      :priority,
-      :due_date,
-      :status,
-      :detail
-    )
-  end
-
 end
