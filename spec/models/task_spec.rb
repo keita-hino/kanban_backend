7aa2063059d118baf1ec9fa95b1a843aa930c088
keita <keitahino0415@gmail.com>
2020-07-26 12:44:02 +0900

    バリデーションのspecリファクタ

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,26 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Task do
   let(:task) { create(:task) }
 
   context 'validation' do
+    subject { task.validate }
+
     context 'name(タスク名)' do
       it '必須になっていること' do
         task.name = nil
-        task.valid?
-        expect(task).to be_invalid
+        is_expected.to be_falsey
       end
       it '101文字以上は入力不可になっていること' do
         task.name = 'a' * 101
-        task.valid?
-        expect(task).to be_invalid
+        is_expected.to be_falsey
       end
     end
     context 'datail(詳細)' do
       it '201文字以上は入力不可になっていること' do
         task.detail = 'a' * 201
-        task.valid?
-        expect(task).to be_invalid
+        is_expected.to be_falsey
       end
     end
   end
