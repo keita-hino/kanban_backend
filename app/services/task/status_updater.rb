c8bee8ace3da1fdfd602923e357b16a77ae4ce65
keita <keitahino0415@gmail.com>
2020-10-04 15:49:50 +0900

    StatusUpdaterのリファクタ

--- app/services/task/status_updater.rb
+++ app/services/task/status_updater.rb
@@ -13,36 +13,50 @@ class Task::StatusUpdater
 
   def call
     ActiveRecord::Base.transaction do
-      # 挿入されたタスク直下のタスク
-      before_moved_task = Task.immediate_task(status, display_order, workspace_id)
-
       # タスクステータス更新処理
       task = Task.find(id)
       task.status = status
       task.save!
 
-      # 同一ステータスの末尾タスクのID取得
-      last_task_id = Task.same_status_last_task_id(status, workspace_id)
+      update_task_status(task)
+    end
+  end
 
-      # 先頭のタスクだった場合は新たに作成したdisplay_order
-      target_display_order = before_moved_task.nil? ? task.display_order : before_moved_task.display_order
+  private
 
-      # 末尾に追加された場合は更新しない
-      if !before_moved_task.nil? && before_moved_task&.id != last_task_id
-        # 追加したタスクより後のタスク取得
-        target_tasks = Task.later_tasks(task.id, status, workspace_id, target_display_order)
+  # 挿入されたタスク直下のタスク
+  def before_moved_task
+    @before_moved_task ||= Task.immediate_task(status, display_order, workspace_id)
+  end
 
-        # 並び更新
-        target_tasks.each.with_index(1) do |target_task, index|
-          target_task.display_order = task.display_order + index
-          target_task.save!
-        end
+  # 末尾タスクのIDを取得
+  def last_task_id
+    @last_task_id ||= Task.same_status_last_task_id(status, workspace_id)
+  end
 
-      else
-        # 末尾にタスクが追加された場合は追加したタスクのdisplay_orderをインクリメントする
-        task.display_order += 1
-        task.save!
+  def add_end_task?
+    !before_moved_task.nil? && before_moved_task&.id != last_task_id
+  end
+
+  # タスクのステータスを更新
+  def update_task_status(task)
+    # 先頭のタスクだった場合は新たに作成したdisplay_order
+    target_display_order = before_moved_task.nil? ? task.display_order : before_moved_task.display_order
+
+    # 末尾に追加された場合は更新しない
+    if add_end_task?
+      # 追加したタスクより後のタスク取得
+      target_tasks = Task.later_tasks(task.id, status, workspace_id, target_display_order)
+
+      # 並び更新
+      target_tasks.each.with_index(1) do |target_task, index|
+        target_task.display_order = task.display_order + index
+        target_task.save!
       end
+    else
+      # 末尾にタスクが追加された場合は追加したタスクのdisplay_orderをインクリメントする
+      task.display_order += 1
+      task.save!
     end
   end
 end
