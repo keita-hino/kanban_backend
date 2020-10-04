c387f5191eb5cc24992312184587acf4636866eb
keita <keitahino0415@gmail.com>
2020-10-04 17:49:51 +0900

    共通部分をモデルに持たせるよう修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
-class Task < ApplicationRecord
+# frozen_string_literal: true
 
+class Task < ApplicationRecord
   ##
   # relations
   ##
@@ -30,44 +31,44 @@ class Task < ApplicationRecord
 
   # タスク名
   validates :name,
-    presence: true,
-    length: { maximum: 100 }
+            presence: true,
+            length: { maximum: 100 }
 
   # 詳細
   validates :detail,
-    length: { maximum: 200 }
+            length: { maximum: 200 }
 
   # 期限日
   validates :due_date,
-    allow_blank: true,
-    date: { presence: true }
+            allow_blank: true,
+            date: { presence: true }
 
   ##
   # scopes
   ##
 
   # not_IDis
-  scope :not_id_is, -> (id) {
+  scope :not_id_is, lambda { |id|
     where.not(id: id)
   }
 
   # ワークスペースID_is
-  scope :workspace_id_is, -> (id) {
+  scope :workspace_id_is, lambda { |id|
     where(workspace_id: id)
   }
 
   # ステータス_is
-  scope :status_is, -> (value) {
+  scope :status_is, lambda { |value|
     where(status: value)
   }
 
   # 表示順_is
-  scope :display_order_is, -> (value) {
+  scope :display_order_is, lambda { |value|
     where(display_order: value)
   }
 
   # 表示順_ge
-  scope :display_order_ge, -> (value) {
+  scope :display_order_ge, lambda { |value|
     where('display_order >= ?', value)
   }
 
@@ -77,40 +78,40 @@ class Task < ApplicationRecord
 
   # 該当タスクの直下のタスク取得
   def self.immediate_task(status, display_order, workspace_id)
-    self.status_is(status)
-        .display_order_is(display_order)
-        .workspace_id_is(workspace_id)
-        .order(:display_order)
-        .last
+    status_is(status)
+      .display_order_is(display_order)
+      .workspace_id_is(workspace_id)
+      .order(:display_order)
+      .last
   end
 
   # 同一ステータスの末尾タスクのID取得
   def self.same_status_last_task_id(status, workspace_id)
-    self.status_is(status)
-        .workspace_id_is(workspace_id)
-        .order(:display_order)
-        .last
-        .id
+    status_is(status)
+      .workspace_id_is(workspace_id)
+      .order(:display_order)
+      .last
+      .id
   end
 
   # 後のタスク取得
   def self.later_tasks(id, status, workspace_id, target_display_order)
-    self.status_is(status)
-        .workspace_id_is(workspace_id)
-        .display_order_ge(target_display_order)
-        .not_id_is(id)
-        .order(:display_order)
+    status_is(status)
+      .workspace_id_is(workspace_id)
+      .display_order_ge(target_display_order)
+      .not_id_is(id)
+      .order(:display_order)
   end
 
   # display_orderをインクリメントする
   def increase_display_order
     self.display_order += 1
-    self.save!
+    save!
   end
 
   # display_orderをデクリメントする
   def decrease_display_order
     self.display_order -= 1
-    self.save!
+    save!
   end
 end
