b5a6845c5e46a527b9c86396ad38143ff431932a
keita <keitahino0415@gmail.com>
2020-10-04 14:56:58 +0900

    OrderUpdaterのリファクタ

--- app/services/task/order_updater.rb
+++ app/services/task/order_updater.rb
@@ -1,50 +1,63 @@
+# frozen_string_literal: true
+
 class Task::OrderUpdater
-  # 初期化
+  attr_accessor :workspace_id, :id, :status, :display_order, :moved_tasks_params
+
   def initialize(workspace_id, moved_tasks_params)
     @workspace_id = workspace_id
+    @id = moved_tasks_params[:id]
+    @status = moved_tasks_params[:status]
+    @display_order = moved_tasks_params[:display_order]
     @moved_tasks_params = moved_tasks_params
   end
 
   # タスクの並び順を変更
   def call
-    status = @moved_tasks_params[:status]
-    display_order = @moved_tasks_params[:display_order]
-
     ActiveRecord::Base.transaction do
-      # 挿入されたタスク直下のタスク
-      before_moved_task = Task.immediate_task(status, display_order, @workspace_id)
-
       # タスクステータス更新処理
-      task = Task.find(@moved_tasks_params[:id])
-      task.assign_attributes(@moved_tasks_params)
+      task = Task.find(id)
+      task.assign_attributes(moved_tasks_params)
       task.save!
 
-      # 同一ステータスの末尾タスクのID取得
-      last_task_id = Task.same_status_last_task_id(status, @workspace_id)
+      # タスクの並び順更新
+      update_order_task(task)
+    end
+  end
 
-      # 先頭のタスクだった場合は新たに作成したdisplay_order
-      target_display_order = before_moved_task.nil? ? task.display_order : before_moved_task.display_order
+  private
 
-      # 末尾に追加された場合は更新しない
-      if !before_moved_task.nil? && before_moved_task&.id != last_task_id
-        # 追加したタスクより後のタスク取得
-        target_tasks = Task.later_tasks(task.id, status, @workspace_id, target_display_order)
+  def before_moved_task
+    @before_moved_task ||= Task.immediate_task(status, display_order, workspace_id)
+  end
 
-        # 並び更新
-        target_tasks.each.with_index(1) do |target_task, index|
-          target_task.display_order = task.display_order + index
-          target_task.save!
-        end
+  def add_end_task?
+    # 同一ステータスの末尾タスクのID取得
+    last_task_id = Task.same_status_last_task_id(status, workspace_id)
+    !before_moved_task.nil? && before_moved_task&.id != last_task_id
+  end
 
-        # 移動前と移動後の間にあるタスクを繰り上げ
-        before_moved_task.display_order -= 1
-        before_moved_task.save!
+  def update_order_task(task)
+    # 先頭のタスクだった場合は新たに作成したdisplay_order
+    target_display_order = before_moved_task.nil? ? task.display_order : before_moved_task.display_order
 
-      else
-        # 末尾にタスクが追加された場合は追加したタスクのdisplay_orderをインクリメントする
-        task.display_order += 1
-        task.save!
+    # 末尾に追加された場合は更新しない
+    if add_end_task?
+      # 追加したタスクより後のタスク取得
+      target_tasks = Task.later_tasks(task.id, status, workspace_id, target_display_order)
+
+      # 並び更新
+      target_tasks.each.with_index(1) do |target_task, index|
+        target_task.display_order = task.display_order + index
+        target_task.save!
       end
+
+      # 移動前と移動後の間にあるタスクを繰り上げ
+      before_moved_task.display_order -= 1
+      before_moved_task.save!
+    else
+      # 末尾にタスクが追加された場合は追加したタスクのdisplay_orderをインクリメントする
+      task.display_order += 1
+      task.save!
     end
   end
 end
